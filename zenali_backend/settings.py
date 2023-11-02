981a5d1f3e843ae2ae60880d32d99d2ba96ee40f
Sajjad <titanxl79@gmail.com>
2023-11-02 08:17:21 +0330

    add mysql backup

--- zenali_backend/settings.py
+++ zenali_backend/settings.py
@@ -51,7 +51,10 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.DjangoModelPermissions',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10,
 
 }
 
