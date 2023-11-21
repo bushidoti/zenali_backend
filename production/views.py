4909c2f045ed21b2abda04a1f3f3aaa0028dddc3
Sajjad <titanxl79@gmail.com>
2023-11-21 16:34:37 +0330

    change view and add filter to pending and production

--- production/views.py
+++ production/views.py
@@ -1,8 +1,17 @@
+import django_filters
 from rest_framework import viewsets
 from rest_framework.exceptions import MethodNotAllowed
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated, BasePermission
 from .models import PendingProduce
 from .serializer import PendingProduceSerializer
+from django_filters import rest_framework as df_filters
+from django_filters.fields import CSVWidget, MultipleChoiceField
+
+
+class CustomPageNumberPagination(PageNumberPagination):
+    page_size_query_param = 'size'
+    page_query_param = 'page'
 
 
 class MyPermission(BasePermission):
@@ -31,9 +40,36 @@ class MyPermission(BasePermission):
         return False
 
 
+class MultipleField(MultipleChoiceField):
+    def valid_value(self, value):
+        return True
+
+
+class MultipleFilter(df_filters.MultipleChoiceFilter):
+    field_class = MultipleField
+
+
+class PendingProduceFilter(django_filters.rest_framework.FilterSet):
+    date = django_filters.rest_framework.CharFilter(field_name='date', lookup_expr='exact')
+    purpose = django_filters.rest_framework.CharFilter(field_name='purpose', lookup_expr='exact')
+    id = django_filters.rest_framework.NumberFilter(field_name='id', lookup_expr='contains')
+    request = django_filters.rest_framework.NumberFilter(field_name='request__id', lookup_expr='contains')
+    status = MultipleFilter(
+        lookup_expr="contains",
+        field_name="status",
+        widget=CSVWidget
+    )
+
+    class Meta:
+        model = PendingProduce
+        fields = ['id', 'purpose', 'status', 'request', 'date']
+
+
 class PendingProduceApi(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, MyPermission]
     perm_slug = "production.pendingproduce"
-
+    pagination_class = CustomPageNumberPagination
     serializer_class = PendingProduceSerializer
     queryset = PendingProduce.objects.all()
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filterset_class = PendingProduceFilter
