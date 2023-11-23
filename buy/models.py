946f266fd014c53fd47d2aeb1bba529072250da1
Sajjad <titanxl79@gmail.com>
2023-11-23 20:18:49 +0330

    left and average price industrial products

--- buy/models.py
+++ buy/models.py
@@ -4,8 +4,8 @@ from django_jalali.db import models as jmodels
 
 class ApplyBuy(models.Model):
     name = models.CharField("نام کالا", max_length=100, blank=False, null=True)
-    scale = models.CharField("واحد", max_length=50, blank=True, null=True)
-    amount = models.FloatField("مقدار", blank=True, null=True)
+    scale = models.CharField("مقیاس", max_length=50, blank=True, null=True)
+    amount = models.FloatField("تعداد", blank=True, null=True)
     applicant = models.CharField("درخواست کننده", max_length=50, blank=False)
     description = models.CharField("توضیحات", default='', max_length=150, blank=False, null=False)
     is_supplied = models.BooleanField("تامین شده ؟", blank=True, null=True)
